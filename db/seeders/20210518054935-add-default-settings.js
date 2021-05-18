a18d6729f11a7364a691dcb6690a5e3513018882
Aditya Toshniwal <aditya.toshniwal14@gmail.com>
2021-05-18 11:22:07 +0530

    Add migration for changing settings.

---
+++ db/seeders/20210518054935-add-default-settings.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.bulkDelete('Settings');
+      await queryInterface.bulkInsert('Settings', [{
+        value: JSON.stringify({
+          "lassa_unit":"meter",
+          "warp_rate_gst": 5,
+          "sizing_rate_gst": 5,
+          "weft_rate_gst": 5
+        }),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }]);
+    } catch(e) {
+      console.log('Error occurred', e.message);
+      throw 'Database error';
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
